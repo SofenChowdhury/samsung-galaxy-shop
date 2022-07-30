4f4e2bf6e616ebedc3a6ae7c1fbcb55f790b02bd
Chowdhury Md. Rabby Mahmud <md.rabby.mahmud@gmail.com>
2022-07-30 17:07:39 +0600

    Create components

---
+++ components/SamsungBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SamsungBanner = () => {
+  return (
+    <div>SamsungBanner</div>
+  )
+}
+
+export default SamsungBanner
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      Samsung Banner
+
+      <div className='products-heading'>
+        <h2>Beset Selling Products</h2>
+        <p>Speaker of many variations</p>
+      </div>
+
+      <div className='products-container'>
+        {
+          ['P_1', 'P_2'].map(
+            (product) => product
+          )
+        }
+      </div>
+
+      Footer
+    </>
   )
 }
 
-export default index
+export default Home
